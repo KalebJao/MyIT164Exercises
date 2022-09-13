be959a20fcdd4d34062d48f7705cd51cb8de5e06
KalebJao <112100262+KalebJao@users.noreply.github.com>
2022-09-13 10:39:53 -0500

    Add files via upload

---
+++ Modules_KalebJ.py
@@ -0,0 +1,18 @@
+import Birthday_KalebJ
+
+print ('\n\nNow let\'s talk about your major')
+
+majorAbbr= input('pick any of the following: CS, GIS, IS, HIM, or NA: ')
+
+if majorAbbr =='CS':
+        major='Computer Science'
+if majorAbbr=='IS':
+        major='Information Systems09'
+if majorAbbr=='HIM':
+    major='Health Informatics and Management'
+if majorAbbr=='GIS':
+    major='Geographic Information Science'
+if major=='NA':
+    major= 'Undergraduate College'
+
+print('you\'re majoring in', major)
