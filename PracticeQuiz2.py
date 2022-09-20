1cea972cdedba86d6d8f7002527cd3f0775b1cf9
KalebJao <112100262+KalebJao@users.noreply.github.com>
2022-09-20 10:21:18 -0500

    Add files via upload
    MyPracticeQuiz Attempt

---
+++ PracticeQuiz2.py
@@ -0,0 +1,13 @@
+List1= int(input('Give a number:'))
+List2= int(input('Give a second number:'))
+List3= int(input('Give a third number'))
+
+
+
+numList = [1,2,3]
+
+
+Average= (numList(1), numList(2), numList(3)) / 3
+
+
+print= Average
